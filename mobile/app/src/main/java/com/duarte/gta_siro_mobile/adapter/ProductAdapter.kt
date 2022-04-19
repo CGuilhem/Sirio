1b053a17cf326d70614c9c0a9a582884d2a44396
Thomas Duarte <duarte.thomas1301@gmail.com>
2022-04-19 20:46:43 +0200

    affichage de 8 produits sous forme de liste

---
+++ mobile/app/src/main/java/com/duarte/gta_siro_mobile/adapter/ProductAdapter.kt
@@ -0,0 +1,26 @@
+package com.duarte.gta_siro_mobile.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.duarte.gta_siro_mobile.R
+
+class ProductAdapter : RecyclerView.Adapter<ProductAdapter.ViewHolder>(){
+    //Boite permettant de ranger les composants
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        //image du produit
+        val productPicture = view.findViewById<ImageView>(R.id.imageItem)
+    }
+    //Permet d'injecter notre layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vertical_product, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { }
+
+    //Permet de générer le nombre d'item désiré
+    override fun getItemCount(): Int = 8
+}
--- mobile/app/src/main/java/com/duarte/gta_siro_mobile/fragments/HomeFragment.kt
+++ mobile/app/src/main/java/com/duarte/gta_siro_mobile/fragments/HomeFragment.kt
@@ -1,16 +1,29 @@
 package com.duarte.gta_siro_mobile.fragments
 
+import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.duarte.gta_siro_mobile.R
+import com.duarte.gta_siro_mobile.adapter.ProductAdapter
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+        val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.verticalRecyclerView)
 
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        //Charger les plantes
+        verticalRecyclerView.adapter = ProductAdapter()
+
+        return view
     }
 }
