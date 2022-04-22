f5cf94f4c6988660b90bb1a56ea69db446d7ae39
Nicolas <arnaud.nicolas@supdevinci-edu.fr>
2022-04-22 12:04:58 +0200

    Inscription

--- frontend/src/app/body/inscription/inscription.component.ts
+++ frontend/src/app/body/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  form: any;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.form = document.querySelector('#connexionForm');
+    this.form.addEventListener('submit', this.handleSubmit);
   }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+
+    const data = new FormData(this.form);
+    const json = Object.fromEntries((<any>data).entries());
+
+    console.log(json);
+    if (json.confPassword == json.password) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://192.168.20.145:3000/api/auth/signup');
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("Content-type", "application/json");
+
+      xhr.send(JSON.stringify(json));
+    } else {
+      console.log("mot de passe non similaire")
+    }
+  }
+
 
 }
