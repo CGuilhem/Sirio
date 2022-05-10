9be737d433bc1eecf4e83be224410ce3173f6c37
CGuilhem <guilhemcalonne@yahoo.fr>
2022-05-10 10:59:13 +0200

    [FIX] updateMeuble

--- backend/Controllers/meuble.js
+++ backend/Controllers/meuble.js
@@ -73,7 +73,17 @@ exports.deleteMeuble = (req, res, next) => {
 exports.updateMeuble = (req, res, next) => {
     console.log("Requête updateMeuble");
 
-    Meuble.updateOne({ _id: req.params.id }, { ...req.body.meuble })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
+    Meuble.findOne({ _id: req.params.id  })
+    .then(meuble => {
+        if (!meuble) {
+          res.status(404).json({
+            error: 'Aucun meuble trouvé !'
+          });
+        } else {
+            Meuble.updateOne({ _id: req.params.id }, { ...req.body.meuble })
+                .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+                .catch(error => res.status(400).json({ error }));
+        }
+    })
+    .catch(error => res.status(500).json({ error: `Erreur lors de la modification d'un meuble` }));
 };
