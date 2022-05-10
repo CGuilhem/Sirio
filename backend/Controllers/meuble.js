7386ac009e6925340d2c51576a14bd75f2dfb12d
CGuilhem <guilhemcalonne@yahoo.fr>
2022-05-10 10:36:21 +0200

    [FIX] deleteMeuble

--- backend/Controllers/meuble.js
+++ backend/Controllers/meuble.js
@@ -52,11 +52,10 @@ exports.deleteMeuble = (req, res, next) => {
         .then(meuble => {
             if (!meuble) {
                 res.status(404).json({
-                error: new Error('Aucun meuble trouvé !')
+                error: 'Aucun meuble trouvé !'
                 });
-            }
-        
-            /*const filename = thing.imageUrl.split('/Images/')[1];
+            } else {
+                /*const filename = thing.imageUrl.split('/Images/')[1];
             fileSystem.unlink(`images/${filename}`, () => {
                 Meuble.deleteOne({ _id: req.params.id })
                 .then(() => res.status(200).json({ message: 'Meuble supprimé !'}))
@@ -64,8 +63,9 @@ exports.deleteMeuble = (req, res, next) => {
             });*/
 
             Meuble.deleteOne({ _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'Meuble supprimé !'}))
-                .catch(error => res.status(400).json({ error }));
+            .then(() => res.status(200).json({ message: 'Meuble supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
+            }       
         })
         .catch(error => res.status(500).json({ error }));
 };
