b18322162e493a608cb29e7f2d848b74f3facdda
CGuilhem <guilhemcalonne@yahoo.fr>
2022-05-11 16:40:03 +0200

    Update payment.js

--- backend/Controllers/payment.js
+++ backend/Controllers/payment.js
@@ -4,8 +4,8 @@ const res = require('express/lib/response');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 const storeItems = new Map([
-    [1, { priceInCents: 10000, name: "Learn React Today" }],
-    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+    [1, { priceInCents: 10000, name: "Chaise SIRIO" }],
+    [2, { priceInCents: 20000, name: "Bureau SIRIO" }],
 ])
 
 exports.createCheckoutSession = async (req, res, next) => {
