1c1cf86a81b192f6d9061fa761cd8b526395ef7a
Thomas1301 <51739765+Thomas1301@users.noreply.github.com>
2022-05-28 21:34:01 +0200

    finalisation page recherche

--- mobile/app/src/main/java/com/duarte/gta_siro_mobile/fragments/SearchFragment.kt
+++ mobile/app/src/main/java/com/duarte/gta_siro_mobile/fragments/SearchFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.duarte.gta_siro_mobile.MainActivity
 import com.duarte.gta_siro_mobile.R
+import com.duarte.gta_siro_mobile.adapter.ProductAdapter
 
 class SearchFragment(private val context: MainActivity) : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view = inflater?.inflate(R.layout.fragment_favorites, container, false)
+        val view = inflater?.inflate(R.layout.search_fragment, container, false)
 
         return view
     }
